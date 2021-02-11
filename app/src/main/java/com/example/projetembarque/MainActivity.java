12ef6cbd5a73e9acca33b7df2c318dfa15b78746
YannPlmr <paulmery.yann@hotmail.fr>
2021-02-11 17:45:48 +0100

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/projetembarque/MainActivity.java
+++ app/src/main/java/com/example/projetembarque/MainActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import java.util.Objects;
@@ -19,7 +24,31 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickConnect(View view) {
         Intent connectionIndent = new Intent(getApplicationContext(), ConnectionActivity.class);
-        startActivityForResult(connectionIndent, 1);
+        //startActivityForResult(connectionIndent, 1);
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.test_popup, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
     }
 
     public void onClickRoom(View view) {
