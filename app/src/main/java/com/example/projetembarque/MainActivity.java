4093e99360d24a54eca47b3acc167bd0023b4b97
YannPlmr <paulmery.yann@hotmail.fr>
2021-02-18 16:11:43 +0100

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/example/projetembarque/MainActivity.java
+++ app/src/main/java/com/example/projetembarque/MainActivity.java
@@ -10,11 +10,10 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
-import android.widget.Toast;
-
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    //TODO : Condition pour afficher soit le bouton de connexion, soit le bouton de profil
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +23,43 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickConnect(View view) {
         Intent connectionIndent = new Intent(getApplicationContext(), ConnectionActivity.class);
-        //startActivityForResult(connectionIndent, 1);
 
         // inflate the layout of the popup window
-        LayoutInflater inflater = (LayoutInflater)
-                getSystemService(LAYOUT_INFLATER_SERVICE);
-        View popupView = inflater.inflate(R.layout.test_popup, null);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.pop_up_login, null);
 
         // create the popup window
         int width = LinearLayout.LayoutParams.WRAP_CONTENT;
         int height = LinearLayout.LayoutParams.WRAP_CONTENT;
         boolean focusable = true; // lets taps outside the popup also dismiss it
-        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, true);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
+    }
+
+    public void onClickJoin(View view) {
+        Intent connectionIndent = new Intent(getApplicationContext(), ConnectionActivity.class);
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.pop_up_join, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, true);
 
         // show the popup window
         // which view you pass in doesn't matter, it is only used for the window tolken
